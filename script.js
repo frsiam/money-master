402b2f75a2ea7d18f76f42dda2687bee1f4ea08b
frsiam <siam.frs@gmail.com>
2022-02-17 11:48:23 +0600

    create function for calculation and call the function from onclick by calculate button with error message handling if have string input

--- script.js
+++ script.js
@@ -7,20 +7,37 @@ function calculateBalance(){
     //Income input field
     const incomeField = idCatcher('income').value;
     const incomeInput = parseFloat(incomeField);
+    if(isNaN(incomeField)){
+        idCatcher('income-error').style.display = 'block';
+    }
     //Expensess input field
+    //For food cost
     const foodField = idCatcher('food').value;
     const foodInput = parseFloat(foodField);
-
+    if(isNaN(foodField)){
+        idCatcher('food-error').style.display = 'block';
+    }
+    //For rent cost
     const rentField = idCatcher('rent').value;
     const rentInput = parseFloat(rentField);
-    // console.log(rentField)
+    if(isNaN(rentField)){
+        idCatcher('rent-error').style.display = 'block';
+    }
 
+    //For cloth cost
     const clothField = idCatcher('cloths').value;
     const clothInput = parseFloat(clothField);
+    if(isNaN(clothField)){
+        idCatcher('cloth-error').style.display = 'block';
+    }
     /* if(isNaN(clothInput)){
         return alert('thik ase')
     }
     else{
         return alert('sorry')
     } */
+    idCatcher('income').value = '';
+    idCatcher('food').value = '';
+    idCatcher('rent').value = '';
+    idCatcher('cloths').value = '';
 }
